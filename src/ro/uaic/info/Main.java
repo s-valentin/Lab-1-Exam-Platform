f4e2801a46a76cbedd56f6fceeb7ba07c0d62c06
Vali <56320456+JoggingMist@users.noreply.github.com>
2021-02-16 12:51:58 +0200

    testpushvali

--- src/ro/uaic/info/Main.java
+++ src/ro/uaic/info/Main.java
@@ -3,6 +3,6 @@ package ro.uaic.info;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Na beleaua");
+        System.out.println("testpushvali");
     }
 }
