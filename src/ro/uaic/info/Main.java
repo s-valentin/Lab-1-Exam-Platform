290d4d8fcc3b09fa05333ce8a5d969ecc5238323
Vali <56320456+JoggingMist@users.noreply.github.com>
2021-02-19 20:09:06 +0200

    reparare

--- src/ro/uaic/info/Main.java
+++ src/ro/uaic/info/Main.java
@@ -7,35 +7,8 @@ public class Main {
 
     public static void main(String[] args) {
 
-
-        question.setTitle("Alabama");
-        question.setType(QuestionType.shortAnswer);
-        question.setPoints(6);
-
-        System.out.println(question.getTitle());
-
-        String raspuns = "";
-        System.out.println("Introduceti raspunsurile corecte \nLa final puneti 0");
         Scanner scanner = new Scanner(System.in);
         ArrayList<Exam> exams = new ArrayList<>();
-
-        System.out.println("Introduceti raspunsurile gresite \nLa final puneti 0");
-        raspuns = scanner.next();
-        while(!raspuns.equals("0"))
-        {
-            question.setWrongAnswers(raspuns);
-            raspuns = scanner.next();
-        }
-
-        question.createAllAnswers();
-        System.out.println(question.nbAllAnswers);
-
-        question.print();
-
-        System.out.println("Verificare raspunsuri");
-        raspuns = scanner.next();
-        question.checkInputAnswer(raspuns);
-
 
         String input;
 
