7ad8c1be3a2e31a51bb7e2b53e316275f4effcb2
Vali <56320456+JoggingMist@users.noreply.github.com>
2021-02-19 19:53:42 +0200

    question class done

--- src/ro/uaic/info/Main.java
+++ src/ro/uaic/info/Main.java
@@ -9,13 +9,13 @@ public class Main {
         Questions question = new Questions();
 
         question.setTitle("Alabama");
-        question.setType(QuestionType.dropdown);
-        question.setPoints(10);
+        question.setType(QuestionType.shortAnswer);
+        question.setPoints(6);
 
         System.out.println(question.getTitle());
 
         String raspuns = "";
-        System.out.println("Introduceti ");
+        System.out.println("Introduceti raspunsurile corecte \nLa final puneti 0");
         Scanner scanner = new Scanner(System.in);
         raspuns = scanner.next();
         while(!raspuns.equals("0"))
@@ -24,48 +24,23 @@ public class Main {
             raspuns = scanner.next();
         }
 
+        System.out.println("Introduceti raspunsurile gresite \nLa final puneti 0");
+        raspuns = scanner.next();
+        while(!raspuns.equals("0"))
+        {
+            question.setWrongAnswers(raspuns);
+            raspuns = scanner.next();
+        }
+
+        question.createAllAnswers();
         System.out.println(question.nbAllAnswers);
 
         question.print();
 
-        /*
-        Scanner scanner = new Scanner(System.in);
-        String test = scanner.next();
-        System.out.println(test);
-        test = scanner.next();
-        System.out.println(test);
-        test = scanner.next();
-        System.out.println(test);
-        */
-        String input;
+        System.out.println("Verificare raspunsuri");
+        raspuns = scanner.next();
+        question.checkInputAnswer(raspuns);
 
-        int profCount = 0;
-        int studCount = 0;
-
-        Professor[] profs = new Professor[10];
-        Student[] students = new Student[10];
-
-        while (true) {
-            System.out.println("State your identity:");
-            input = scanner.next();
-            for (int i = 0; i < 2; i++) {
-                if (input.equals("Professor")) {
-                    profs[profCount] = new Professor();
-                    profs[profCount++].createExam();
-                    break;
-                } else if (input.equals("Student")) {
-                    System.out.println(input);
-                    students[studCount] = new Student();
-                    students[studCount++].chooseExam("OOP");
-                    break;
-                } else if (i == 0) {
-                    System.out.println("Are you a student or a professor?");
-                    input = scanner.next();
-                } else if (i == 1) {
-                    System.out.println("Bye bye!");
-                }
-            }
-        }
 
     }
 }
